0a988467ebe1ef44711e99dfb9b7430b7ebb577d
Sean LaFlam <sean.laflam@gmail.com>
2020-08-08 02:03:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,11 +8,11 @@ def valid_move?(board, index)
   end
 end
 
-# def position_taken?(board, index)
-#   if (board[index] == "X" || board[index] == "O")
-#     true
-#   else
-#     false 
-#   end
-# end
+def position_taken?(board, index)
+  if (board[index] == "X" || board[index] == "O")
+    true
+  else
+    false 
+  end
+end
   
