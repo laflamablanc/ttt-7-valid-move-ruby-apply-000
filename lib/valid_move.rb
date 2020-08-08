e03abe6bd0417ac4f83dd91ee5d990553ad83c5c
Sean LaFlam <sean.laflam@gmail.com>
2020-08-08 02:01:12 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,10 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == false
-     return true
+  value = position_taken?(board,index)
+  return value
+  # if position_taken?(board, index) == false
+  #   return true
   # else
   #   return false 
   # end
