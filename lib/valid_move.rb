3458fe6f3b9a751503074a6e1d00b06cc6e6e61c
Sean LaFlam <sean.laflam@gmail.com>
2020-08-08 01:54:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, num)
-  if num < 9
-    if position_taken?(board, num) == false
+  if num < 9 && position_taken?(board, num) == false
       return true
     end
   else
